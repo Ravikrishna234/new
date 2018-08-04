ce71842c05a077d5d5feb9330d1416b78e4ea17b
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-04 10:08:58 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignment/M5/p4/square_root_newtonrapson.py
+++ cspp1-assignment/M5/p4/square_root_newtonrapson.py
@@ -7,5 +7,6 @@ def main():
     while abs(sta_rt*sta_rt - sq_in) >= cons_tant:
         sta_rt = sta_rt - (((sta_rt**2) - sq_in)/(2*sta_rt))
     print(str(sta_rt))
+
 if __name__ == "__main__":
     main()
