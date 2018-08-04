a700fa29b07589a418a0dff3be3c9c6cb039ea14
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-04 10:17:43 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- cspp1-assignment/m6/p1/fizz_buzz.py
+++ cspp1-assignment/m6/p1/fizz_buzz.py
@@ -7,9 +7,10 @@ def main():
             print("Fizz")
         elif i % 5 == 0:
             print("Buzz")
-        elif i % 3 == 0 and i % 5 == 0:
-            print("Fizz\n")
-            print("Buzz")
+        elif i % 3 == 0
+            print("Fizz")
+            if i % 5 == 0
+                print("Buzz")
         else:
             print(i)
 if __name__ == "__main__":
