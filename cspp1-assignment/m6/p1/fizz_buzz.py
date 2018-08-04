0079fa263be0877d4e3d96e0ce21f58df3bc654c
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-04 10:20:56 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignment/m6/p1/fizz_buzz.py
+++ cspp1-assignment/m6/p1/fizz_buzz.py
@@ -3,14 +3,13 @@ def main():
     """Fizz"""
     n_in = int(input())
     for i in range(1, n_in+1):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             print("Fizz")
+            print("Buzz")
         elif i % 5 == 0:
             print("Buzz")
         elif i % 3 == 0:
             print("Fizz")
-            if i % 5 == 0:
-                print("Buzz")
         else:
             print(i)
 if __name__ == "__main__":
