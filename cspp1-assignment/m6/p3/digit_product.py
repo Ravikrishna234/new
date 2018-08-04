81210df47f73181071da7ad59e428364026cf73e
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-04 10:58:50 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- cspp1-assignment/m6/p3/digit_product.py
+++ cspp1-assignment/m6/p3/digit_product.py
@@ -2,6 +2,10 @@
 def main():
     """digit"""
     num_in = int(input())
+    if num_in < 0
+        num_in = -num_in
+    elif num_in == 0
+        product_st = 0
     product_st = 1
     while num_in > 0:
         di_git = num_in % 10
