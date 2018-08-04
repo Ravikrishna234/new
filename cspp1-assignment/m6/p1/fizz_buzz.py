590924094bbc5d2289e5670ae5b83984c6f1284a
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-04 10:17:58 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- cspp1-assignment/m6/p1/fizz_buzz.py
+++ cspp1-assignment/m6/p1/fizz_buzz.py
@@ -7,9 +7,9 @@ def main():
             print("Fizz")
         elif i % 5 == 0:
             print("Buzz")
-        elif i % 3 == 0
+        elif i % 3 == 0:
             print("Fizz")
-            if i % 5 == 0
+            if i % 5 == 0:
                 print("Buzz")
         else:
             print(i)
