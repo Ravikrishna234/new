b589b33dc86354105f415fdbd33235c4c2cb9662
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-04 11:17:35 +0530

    " -> 0 of 7 passed. pylint: 6.67/10 "

--- cspp1-assignment/m6/p3/digit_product.py
+++ cspp1-assignment/m6/p3/digit_product.py
@@ -5,16 +5,17 @@ def main():
     temp_in = num_in
     if num_in < 0:
         num_in = -num_in
-    product_st = 1
-    while num_in > 0:
-        di_git = num_in % 10
-        product_st = product_st * di_git
-        num_in = num_in // 10
-        if temp_in < 0:
-            product_st=-product_st
-        elif temp_in == 0:
-            print(temp_in)
-        else:
-            print(product_st)
+    elif num_in == 0:
+        print(num_in)
+        break
+    else:
+        product_st = 1
+        while num_in > 0:
+            di_git = num_in % 10
+            product_st = product_st * di_git
+            num_in = num_in // 10
+            if temp_in < 0:
+                product_st=-product_st
+        print(product_st)
 if __name__ == "__main__":
     main()
