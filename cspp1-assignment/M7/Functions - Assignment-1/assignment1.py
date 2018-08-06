e4b7d89192d0eb26c3f693b8b816966d8d1fb648
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-06 19:10:06 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignment/M7/Functions - Assignment-1/assignment1.py
+++ cspp1-assignment/M7/Functions - Assignment-1/assignment1.py
@@ -3,11 +3,13 @@ def paying_debtoff(balance, annual_interest_rate, monthly_payment_rate):
     """balance"""
     monthly_interest_rate = annual_interest_rate/12.0
     updated_balance = balance
-    for i in range(1, 13):
+    month_ = 1
+    while month_ <= 12:
         minimum_monthly_payment = monthly_payment_rate * updated_balance
         monthly_unpaid_balance = updated_balance - minimum_monthly_payment
         updated_balance = monthly_unpaid_balance + (monthly_unpaid_balance*monthly_interest_rate)
         p_in = round(updated_balance, 2)
+        month_ += 1
     print("Remaining balance:", p_in)
 def main():
     """balance"""
