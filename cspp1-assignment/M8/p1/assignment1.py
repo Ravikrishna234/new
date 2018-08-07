8b348bd9f4b6a14e1790307076cedea72cc84b1e
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-07 14:38:33 +0530

    " -> 9 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignment/M8/p1/assignment1.py
+++ cspp1-assignment/M8/p1/assignment1.py
@@ -3,8 +3,7 @@ def factorial(n_input):
     """fact"""
     if n_input == 1:
         return 1
-    else:
-        return n_input * factorial(n_input - 1)
+    return n_input * factorial(n_input - 1)
 def main():
     """fact"""
     a_input = input()
