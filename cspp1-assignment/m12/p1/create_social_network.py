3b93514db0f72ed95b00c080e20492bc60d1a22a
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-11 10:48:42 +0530

    " -> 3 of 3 passed. pylint: 9.38/10 "

--- cspp1-assignment/m12/p1/create_social_network.py
+++ cspp1-assignment/m12/p1/create_social_network.py
@@ -11,12 +11,10 @@ def main():
     for i in range(int(lines_)):
         string_ = input()
         index_ = string_.split()
-        if index_[0] not in dictionary_:
-            if index_[1] == "follows":
-                    dictionary_[index_[0]] = index_[2].split(",")
-        else:
-            if index_[1] == "follows":
-                    dictionary_[index_[0]].append(index_[2].split(","))
+        if index_[0] not in dictionary_ and index_[1] == "follows":
+            dictionary_[index_[0]] = index_[2].split(",")
+        elif index_[1] == "follows":
+            dictionary_[index_[0]].append(index_[2].split(","))
     print(create_social_network(dictionary_))
 
 if __name__ == "__main__":
