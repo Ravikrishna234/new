a491fbc52ec87e0277b7dab06b450208dadc49d9
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-11 12:53:59 +0530

    " -> 2 of 2 passed. pylint: 9.68/10 "

--- cspp1-assignment/m12/p2/social_network.py
+++ cspp1-assignment/m12/p2/social_network.py
@@ -1,14 +1,7 @@
 """SOCIALNETWORKING"""
 
 def follow(network, arg1, arg2):
-    """
-        3 arguments are passed to this function
-        network is a dictionary representing the social network
-        arg1 and arg2 are two people in the network
-        follow function is called when arg1 wants to follows arg2
-        so, this should result in adding arg2 to the followers list of arg1
-        update the network dictionary and return it
-    """
+    """social"""
     if arg1 not in network:
         network[arg1] = [arg2]
     else:
@@ -16,27 +9,12 @@ def follow(network, arg1, arg2):
     return network
 
 def unfollow(network, arg1, arg2):
-    """
-        3 arguments are passed to this function
-        network is a dictionary representing the social network
-        arg1 and arg2 are two people in the network
-        unfollow function is called when arg1 wants to stop following arg2
-        so, this should result in removing arg2 from the followers list of arg1
-        update the network dictionary and return it
-    """
+    """social"""
     network[arg1].remove(arg2)
     return network
 
 def delete_person(network, arg1):
-    """
-        2 arguments are passed to this function
-        network is a dictionary representing the social network
-        arg1 is a person in the network
-        delete_person function is called when arg1 wants to exit from the network
-        so, this should result in deleting arg1 from network
-        also, before deleting arg1, remove arg1 from the everyone's followers list
-        update the network dictionary and return it
-    """
+    """social"""
     for i in network.values():
         if arg1 in i:
             i.remove(arg1)
