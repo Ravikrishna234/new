53fd2d47f5d13d9fc145e03a181b540c120fd498
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-16 10:51:13 +0530

    " -> 16 of 19 passed. pylint: 8.30/10 "

--- cspp1-assignment/M14/poker.py
+++ cspp1-assignment/M14/poker.py
@@ -14,11 +14,10 @@ def is_straight(hand):
 def is_flush(hand):
     """POKER"""
     for i in range(len(hand)):
-    suit = hand[0]
-    for h_input in hand:
-        if suit[1] != h_input[1]:
-            print(suit[1])
-            return False
+        suit = hand[0]
+        for h_input in hand:
+            if suit[1] != h_input[1]:
+                return False
     return True
 def four_of_a_kind(hand):
     """POKER"""
