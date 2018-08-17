e40bda71f89eab904f5dd545ad6a28d4260c1301
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-17 16:37:49 +0530

    " -> 0 of 10 passed. pylint: 9.64/10 "

---
+++ cspp1-assignment/M16/CodeCampDocumentDistance/newdocument_distance.py
@@ -0,0 +1,1 @@
+def cleanup_words(input1):
--- cspp1-assignment/M7/Functions - Assignment-3/assignment3.py
+++ cspp1-assignment/M7/Functions - Assignment-3/assignment3.py
@@ -1,10 +1,10 @@
 """Assignment - 3"""
-def paying_debt(balance_1, annual_interest_rate):
+def paying_debt(balance, annual_interest_rate):
     """Bisection Meth"""
     monthly_interest_rate = (annual_interest_rate) / 12.0
-    month_payment_low_bound = balance_1 / 12
-    month_payment_up_bound = (balance_1 * (1 + monthly_interest_rate)**12) / 12.0
-    new_balance = balance_1
+    month_payment_low_bound = balance / 12.0
+    month_payment_up_bound = (balance * (1 + monthly_interest_rate)**12) / 12.0
+    new_balance = balance
     epsilon_ = 0.0001
     guess_ = (month_payment_low_bound + month_payment_up_bound)/2.0
     while True:
@@ -15,14 +15,13 @@ def paying_debt(balance_1, annual_interest_rate):
             month_ += 1
         if new_balance > 0 and new_balance > epsilon_:
             month_payment_low_bound = guess_
-            new_balance = balance_1
-        elif new_balance < 0 and new_balance < epsilon_:
+            new_balance = balance
+        elif new_balance < 0 and new_balance < -epsilon_:
             month_payment_up_bound = guess_
-            new_balance = balance_1
+            new_balance = balance
         else:
             return round(guess_, 2)
-
-        gu_ess = (month_payment_low_bound + month_payment_up_bound)/2
+        guess_ = (month_payment_low_bound + month_payment_up_bound)/2
 
 def main():
     """Bisection Meth"""
@@ -30,6 +29,6 @@ def main():
     # data = "4773 0.2"
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment: "+str(paying_debt(data[0], data[1])))
+    print("Lowest Payment: ",str(paying_debt(data[0], data[1])))
 if __name__ == "__main__":
     main()
