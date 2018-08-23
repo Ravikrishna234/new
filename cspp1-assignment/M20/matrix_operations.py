439090a2f51bf2703d09bb04408ebee557d38155
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-23 17:36:28 +0530

    " -> 0 of 8 passed. pylint: 9.23/10 "

--- cspp1-assignment/M20/matrix_operations.py
+++ cspp1-assignment/M20/matrix_operations.py
@@ -9,7 +9,7 @@ def mult_matrix(m1_, m2_):
     '''
     result = [[0 for i in range(len(m1_))]for j in range(len(m2_[1]))]
     if len(m1_[0]) == len(m2_):
-        for i in range(len(m1_)):
+        for i in enumerate(len(m1_)):
             for j in range(len(m2_[1])):
                 for k in range(len(m2_)):
                     result[i][j] += int(m1_[i][k]) * int(m2_[k][j])
@@ -28,15 +28,14 @@ def add_matrix(m1_, m2_):
     for i in range(len(m1_)):
         if len(m1_) == len(m2_) and len(m1_[i]) == len(m2_[i]):
             result = [[0 for j in range(len(m1_))]for i in range(len(m1_[0]))]
-            result = [[int(m1_[i][j]) + int(m2_[i][j]) for j in range(len(m1_[0]))]for i in range(len(m1_))]
+            result = [[int(m1_[i][j]) + int(m2_[i][j])for j in range(len(m1_[0]))]for i in range(len(m1_))]
     # for i in range(len(m1)):
     #     for j in range(len(m2)):
     #         result[i][j] += int(m1[i][j]) + int(m2[i][j])
             return result
     print("Error: Matrix shapes invalid for addition")
     return None
-
-def read_matrix(l1):
+def read_matrix(matrix_1):
     '''
         read the matrix dimensions from input
         create a list of lists and read the numbers into it
@@ -45,9 +44,9 @@ def read_matrix(l1):
         error message should be "Error: Invalid input for the matrix"
     '''
     matrix = []
-    for i in range(0, int(l1[0])):
+    for _ in range(0, matrix_1[0]):
         column = input().split()
-        if len(column) == int(l1[1]):
+        if len(column) == int(matrix_1[1]):
             matrix.append(column)
         else:
             print("Error: Invalid input for the matrix")
@@ -55,6 +54,7 @@ def read_matrix(l1):
     return matrix
 
 def main():
+    """Take Input"""
     # row1,col1 = map(int,input().split(','))
     # l=[]
     # l1=[]
@@ -62,7 +62,7 @@ def main():
     #     c=input().split()
     #     l.append(c)
     # row2,col2 = map(int,input().split(','))
-    # for i in range(1,row2+1): 
+    # for i in range(1,row2+1):
     #     c1=input().split()
     #     l1.append(c1)
     # print(add_matrix(l,l1))
@@ -73,9 +73,8 @@ def main():
     input_ = input()
     list2_ = input_.split(',')
     matrix_2 = read_matrix(list2_)
-    if matrix_1 != None and matrix_2 != None:
+    if matrix_1 is not None and matrix_2 is not None:
         print(add_matrix(matrix_1, matrix_2))
         print(mult_matrix(matrix_1, matrix_2))
 if __name__ == '__main__':
     main()
-
