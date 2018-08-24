7e62f656d2369f2c69de2f93d8bd08c50c8e91d5
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-24 11:26:54 +0530

    " -> 14 of 14 passed. pylint: 9.67/10 "

--- cspp1-assignment/M21/tic_tac_toe.py
+++ cspp1-assignment/M21/tic_tac_toe.py
@@ -20,6 +20,7 @@ def tic_tac(matrix):
             print(player_[0])
         else:
             print("invalid input")
+            return player_[0]
     else:
         print("invalid game")
         return None
