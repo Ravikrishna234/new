b7d19d69c3d67f6a2ec48bcc1bbd7474384199d5
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-24 11:08:16 +0530

    " -> 0 of 14 passed. pylint: -3.21/10 "

--- cspp1-assignment/M21/tic_tac_toe.py
+++ cspp1-assignment/M21/tic_tac_toe.py
@@ -3,35 +3,28 @@ def main():
 	for i in range(0,3):
 		c=input().split(' ')
 		matrix.append(c)
-	k = []
-	k.append(tictac(matrix))
-	if len(k) == 0:
+	print(tictac(matrix))
+def tictac(matrix):
+	player_= []
+	for i in matrix:
+		if i[0] == i[1] == i[2]:
+			player_.append(i[0])
+	for i in range(0,3):
+		if matrix[0][i] == matrix[1][i] == matrix[2][i]:
+			player_.append(matrix[0][i])
+	if matrix[0][0] == matrix[1][1] == matrix[2][2]:
+		player_.append(matrix[1][1])
+	elif matrix[0][2] == matrix[1][1] == matrix[2][0]:
+		player_.append(matrix[1][1])
+	if len(player_) == 0:
 		print('draw')
-	if len(k) == 1:
-		if k[0] == 'x' or k[0] == 'o':
-			print(k[0])
+		if player_[0] == 'x' or player_[1] == 'o':
+			print(player_[0])
 		else:
 			print("invalid input")
+			return None
 	else:
 		print("invalid game")
-def tictac(matrix):
-	if matrix[0] == matrix[1] == matrix[2]:
-		return matrix[0]
-	elif matrix[0][0] == matrix[1][1] == matrix[2][2]:
-		return matrix[0][0]				
-	elif matrix[0][2] == matrix[1][1] == matrix[2][0]:
-		return matrix[0][2]
-	elif matrix[0][0] == matrix[0][1] == matrix[0][2]:
-		return matrix[0][0]
-	elif matrix[2][0] == matrix[2][1] == matrix[2][2]:
-		return matrix[2][2]
-	elif matrix[0][0] == matrix[1][0] == matrix[2][0]:
-		return matrix[0][0]
-	elif matrix[0][2] == matrix[1][2] == matrix[2][2]:
-		return matrix[2][2]
-	elif matrix[0][1] == matrix[1][1] == matrix[2][1]:
-		return matrix[2][1]
-	elif matrix[1][0] == matrix[1][1] == matrix[1][2]:
-		return matrix[1][2]
+	return player_[0]
 if __name__ == "__main__":
 	main()
