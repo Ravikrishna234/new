fc0af0fd5ba5b7a0c95f69d2f81e6b04875b32ba
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-24 11:24:35 +0530

    " -> 14 of 14 passed. pylint: 9.66/10 "

--- cspp1-assignment/M21/tic_tac_toe.py
+++ cspp1-assignment/M21/tic_tac_toe.py
@@ -1,31 +1,34 @@
+"""TICTACTOE"""
+def tic_tac(matrix):
+    """tictac"""
+    player_ = []
+    for i in matrix:
+        if i[0] == i[1] == i[2]:
+            player_.append(i[0])
+    for i in range(0, 3):
+        if matrix[0][i] == matrix[1][i] == matrix[2][i]:
+            player_.append(matrix[0][i])
+    if matrix[0][0] == matrix[1][1] == matrix[2][2]:
+        player_.append(matrix[1][1])
+    elif matrix[0][2] == matrix[1][1] == matrix[2][0]:
+        player_.append(matrix[1][1])
+    if player_ == []:
+        print("draw")
+        return None
+    if len(player_) == 1:
+        if player_[0] == 'x' or player_[0] == 'o':
+            print(player_[0])
+        else:
+            print("invalid input")
+    else:
+        print("invalid game")
+        return None
 def main():
-	matrix = []
-	for i in range(0,3):
-		c=input().split(' ')
-		matrix.append(c)
-	tictac(matrix)
-def tictac(matrix):
-	player_= []
-	for i in matrix:
-		if i[0] == i[1] == i[2]:
-			player_.append(i[0])
-	for i in range(0,3):
-		if matrix[0][i] == matrix[1][i] == matrix[2][i]:
-			player_.append(matrix[0][i])
-	if matrix[0][0] == matrix[1][1] == matrix[2][2]:
-		player_.append(matrix[1][1])
-	elif matrix[0][2] == matrix[1][1] == matrix[2][0]:
-		player_.append(matrix[1][1])
-	if player_ == []:
-		print("draw")
-		return None
-	if len(player_) == 1:
-		if player_[0] == 'x' or player_[0] == 'o':
-			print(player_[0])
-		else:
-			print("invalid input")
-	else:
-		print("invalid game")
-		return None
+    """tictac"""
+    matrix = []
+    for _ in range(0, 3):
+        list_ = input().split(' ')
+        matrix.append(list_)
+    tic_tac(matrix)
 if __name__ == "__main__":
-	main()
+    main()
