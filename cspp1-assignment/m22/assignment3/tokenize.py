292a74582ad8cb057c081442d31198e749c62bf1
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-25 10:58:45 +0530

    " -> 3 of 4 passed. pylint: -5.00/10 "

--- cspp1-assignment/m22/assignment3/tokenize.py
+++ cspp1-assignment/m22/assignment3/tokenize.py
@@ -3,11 +3,24 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 
-def tokenize(string):
-    pass
+def tokenize(string_):
+	"""tokenize"""
+	aDict={}
+	for i in string_:
+		count_ = string_.count(i)
+		if i not in aDict:
+			aDict[i] = count_
+	return aDict
+		  
+
+	
             
 def main():
-    pass
+	n=int(input())
+	for i in range(n):
+		str_ = input().split(" ")
+	print(tokenize(str_))
+    
 
 if __name__ == '__main__':
     main()
