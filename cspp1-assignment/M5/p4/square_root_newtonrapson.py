a847e46a9d2eed777007660da7f9a50587522f5b
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-08-26 16:20:13 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignment/M5/p4/square_root_newtonrapson.py
+++ cspp1-assignment/M5/p4/square_root_newtonrapson.py
@@ -1,12 +1,12 @@
 """Square root newton rapson"""
 def main():
     """newton"""
-    cons_tant = 0.01
-    sq_in = int(input())
-    sta_rt = sq_in/2.0
-    while abs(sta_rt*sta_rt - sq_in) >= cons_tant:
-        sta_rt = sta_rt - (((sta_rt**2) - sq_in)/(2*sta_rt))
-    print(str(sta_rt))
+    constant_ = 0.01
+    square_input = int(input())
+    startvalue_ = square_input/2.0
+    while abs(startvalue_*startvalue_ - square_input) >= constant_:
+        startvalue_ = startvalue_ - (((startvalue_**2) - square_input)/(2*startvalue_))
+    print(str(startvalue_))
 
 if __name__ == "__main__":
     main()
